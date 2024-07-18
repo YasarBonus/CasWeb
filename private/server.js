1379c31667289696dbf7a0339490cefc9a43e6bf
Joshua Treudler <joshua@treudler.net>
2024-07-18 19:50:11 +0200

    implement winston

--- private/server.js
+++ private/server.js
@@ -7,6 +7,27 @@ require("dotenv").config();
 
 const apiUrl = process.env.NODE_ENV === "production" ? process.env.API_URL_PROD : process.env.API_URL_DEV;
 
+// Winston logger
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
 // EMAILS
 
 const nodemailer = require('nodemailer');
